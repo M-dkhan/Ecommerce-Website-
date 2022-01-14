d3d2b39ed7fe9988332fbf85efc3cecbd26ac0f7
M-dkhan <mdzaidkhan628@gmail.com>
2022-01-14 19:27:19 +0530

    Setup Controller

---
+++ controllers/user.js
@@ -0,0 +1,5 @@
+// All the logic related to the user 
+
+exports.sayHi = (req, res) => {
+    res.json({message:"sayHi method is working"});
+};
--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,10 @@ const express = require('express');
 // Create a new router object
 const router =  express.Router();
 
+//import user controllers 
+const {sayHi} = require('../controllers/user');
 
-router.get('/', (req, res) => {
-    res.send("Hey there, this route is working");
-})
+router.get('/', sayHi);
 
 //  export router as a module
 module.exports = router
